36f498fa8d81869d5b97613daa1794df3e56bbf4
David Dodel <david.dodel@hotmail.com>
2019-04-06 23:14:10 +0200

    Changed paths for tf_ops compilation on workstation

--- pointnet_plusplus/utils/tf_ops/config.sh
+++ pointnet_plusplus/utils/tf_ops/config.sh
@@ -3,9 +3,9 @@
 nvcc_bin=/usr/local/cuda-9.0/bin/nvcc
 
 cuda_include_dir=/usr/local/cuda-9.0/include
-tensorflow_include_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow/include
-tensorflow_external_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
+tensorflow_include_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include
+tensorflow_external_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
 
 cuda_library_dir=/usr/local/cuda-9.0/lib64/
-tensorflow_library_dir=/orions3-zfs/projects/csimstu/venv3.6/lib/python3.6/site-packages/tensorflow
+tensorflow_library_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow
 
