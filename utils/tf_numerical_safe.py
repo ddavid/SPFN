77c5f19f4e1704124342d2b97d64a6216762381e
David Dodel <david.dodel@hotmail.com>
2019-04-07 19:36:13 +0200

    Changed .acos function use for tf-1.8 API

--- utils/tf_numerical_safe.py
+++ utils/tf_numerical_safe.py
@@ -5,4 +5,4 @@ def sqrt_safe(x):
     return tf.sqrt(tf.abs(x) + SQRT_EPS)
 
 def acos_safe(x):
-    return tf.math.acos(tf.clip_by_value(x, -1.0 + 1e-6, 1.0 - 1e-6))
+    return tf.acos(tf.clip_by_value(x, -1.0 + 1e-6, 1.0 - 1e-6))
