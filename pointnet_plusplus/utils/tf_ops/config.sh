d1def68f571f01d11b2ab1de486cbc753c39c6b6
FSD Software <fsd-software@munichmotorsport.de>
2019-04-30 14:53:59 +0200

    Changed CUDA settings to be more general

--- pointnet_plusplus/utils/tf_ops/config.sh
+++ pointnet_plusplus/utils/tf_ops/config.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-nvcc_bin=/usr/local/cuda-9.0/bin/nvcc
+nvcc_bin=/usr/bin/nvcc
 
-cuda_include_dir=/usr/local/cuda-9.0/include
-tensorflow_include_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include
-tensorflow_external_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
+cuda_include_dir=/usr/include
+tensorflow_include_dir=/home/fsd-software/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include
+tensorflow_external_dir=/home/fsd-software/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
 
 cuda_library_dir=/usr/local/cuda-9.0/lib64/
-tensorflow_library_dir=/home/ddodel/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow
+tensorflow_library_dir=/home/fsd-software/miniconda3/envs/spfn/lib/python3.6/site-packages/tensorflow
 
